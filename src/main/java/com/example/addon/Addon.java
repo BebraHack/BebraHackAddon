cdccff4b5aabfa8690fcac839a0e3b07acb51218
rekayno <92271828+rekayno@users.noreply.github.com>
2022-12-08 20:10:19 +0300

    Add files via upload

--- src/main/java/com/example/addon/Addon.java
+++ src/main/java/com/example/addon/Addon.java
@@ -1,7 +1,8 @@
 package com.example.addon;
+import meteordevelopment.meteorclient.systems.modules.Categories;
 import com.example.addon.modules.BebraHackPVP.*;
+import com.example.addon.modules.hud.CustomImage;
 import com.example.addon.modules.hud.Killfeed;
-import com.example.addon.modules.hud.LogoHud;
 import com.example.addon.modules.misc.*;
 import com.example.addon.modules.BebraHackRender.*;
 import com.mojang.logging.LogUtils;
@@ -12,8 +13,10 @@ import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.slf4j.Logger;
 
+
+
 public class Addon extends MeteorAddon {
-    public static String VERSION = "BebraHack V4";
+    public static String VERSION = "BebraHack V5";
     public static String INVITE_LINK = "https://discord.gg/FP8jcqTZyG";
 
     public static final Logger LOG = LogUtils.getLogger();
@@ -21,73 +24,70 @@ public class Addon extends MeteorAddon {
     public static final Category combat = new Category("BebraHackPVP");
     public static final Category misc = new Category("BebraHackPVE");
     public static final Category render = new Category("BebraHackRender");
-    public static final HudGroup HUD_GROUP = new HudGroup("BebraHack+");
-
+    public static final HudGroup HUD_GROUP = new HudGroup("BebraHackHUD");
 
     @Override
     public void onInitialize() {
-        LOG.info("Initializing Meteor Addon Template");
+        LOG.info("Initializing Meteor Addon BebraHack V5");
 
         // HUD
 //		Hud.get().register(ItemCounter.INFO);
         Hud.get().register(Killfeed.INFO);
-        Hud.get().register(LogoHud.INFO);
+        Hud.get().register(CustomImage.INFO);
 //        Hud.get().register(WelcomeHud.INFO);
 //        Hud.get().register(TextPresets.INFO);
 
         // BebraHackPVP
         Modules.get().add(new AntiCrystal());
-        Modules.get().add(new InstaMinePlus());
         Modules.get().add(new Burrow());
         Modules.get().add(new AntiSurroundBlocks());
         Modules.get().add(new FanSurroundBEBRA());
         Modules.get().add(new CevBreaker());
         Modules.get().add(new Notifications());
-        Modules.get().add(new PIRAMIDKA());
-        Modules.get().add(new tntauraBEBRA());
+        Modules.get().add(new tntauraBEBRAplus());
         Modules.get().add(new PacketHoleFill());
-        Modules.get().add(new SurroundV2());
-        Modules.get().add(new AntiTrap());
-        Modules.get().add(new CrystalAyraBEBRA());
+        Modules.get().add(new sphere());
+        Modules.get().add(new AutoEz());
+        Modules.get().add(new SurroundBEBRA());
+        Modules.get().add(new SelfAnvilBEBRA());
         Modules.get().add(new AnchorAuraBEBRA());
+        Modules.get().add(new AntiBurrow());
 
-        // Misc
+
+        // BebraHackPVE
         Modules.get().add(new Twerk());
-        Modules.get().add(new ShulkerDupe());
         Modules.get().add(new AutoBuild());
         Modules.get().add(new NoDesync());
-        Modules.get().add(new SpamPlus());
+        Modules.get().add(new SpamInChat());
         Modules.get().add(new Lavacast());
-        Modules.get().add(new AutoPortalMine());
-        Modules.get().add(new bebra());
+        Modules.get().add(new bebra_info_and_help());
         Modules.get().add(new AutoCraft());
-        Modules.get().add(new GhostBlockFixer());
         Modules.get().add(new VillagerRoller());
         Modules.get().add(new PingSpoofer());
         Modules.get().add(new CoordLogger());
-        Modules.get().add(new AutoBuildSwastika());
         Modules.get().add(new AutoSex());
         Modules.get().add(new TreeAura());
         Modules.get().add(new AutoTNT());
         Modules.get().add(new AutoLogin());
-        Modules.get().add(new SlotClick());
         Modules.get().add(new EFly());
         Modules.get().add(new MEGA_DUPEprank());
-        Modules.get().add(new ChatEncryption());
-        Modules.get().add(new OneClickEat());
+        //Modules.get().add(new ChatEncryption());
         Modules.get().add(new AutoLogTotems());
         Modules.get().add(new SilentItem());
+        Modules.get().add(new Spinner());
 
-        // Render
-        Modules.get().add(new Confetti());
-        Modules.get().add(new HoleESPPlus());
+        // BebraHack Render
+        //Modules.get().add(new Confetti());
+        //Modules.get().add(new HoleESPPlus());
         Modules.get().add(new TimeAnimator());
-        Modules.get().add(new NewChunks());
+        //Modules.get().add(new NewChunks());
         Modules.get().add(new AntiVanish());
-        Modules.get().add(new Prefix());
+        ///Modules.get().add(new Prefix());
         Modules.get().add(new KillEffects());
-        Modules.get().add(new BurrowESP());
-        Modules.get().add(new PenisESP());
+        //Modules.get().add(new BurrowESP());
+        //Modules.get().add(new BebraESP());
+
+        Modules.get().get(bebra_info_and_help.class).toggle();
     }
 
     @Override
